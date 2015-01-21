311501f1ed9f3217366680f9da0b142ae1dda7ce
Martin Zugnoni <martin.zugnoni@gmail.com>
2015-01-21 12:18:54 -0300

    Remove trailing slashs from API urls

--- students_management/api/urls.py
+++ students_management/api/urls.py
@@ -3,9 +3,9 @@ from django.conf.urls import patterns, url
 from . import views
 
 urlpatterns = patterns('',
-    url(r'students/$',
+    url(r'students$',
         views.StudentListCreateAPIView.as_view(), name='students_list'),
 
-    url(r'students/(?P<pk>[0-9]+)/$',
+    url(r'students/(?P<pk>[0-9]+)$',
         views.StudentRetrieveUpdateDestroyAPIView.as_view(), name='students_detail'),
 )
