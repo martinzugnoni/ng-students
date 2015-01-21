a918d51604268c6f1e630ac180b082b1d8f22750
Martin Zugnoni <martin.zugnoni@gmail.com>
2015-01-21 07:56:36 -0300

    Improve API test cases

--- students_management/api/tests/test_api.py
+++ students_management/api/tests/test_api.py
@@ -5,10 +5,17 @@ from students.tests.factories import StudentFactory
 
 class TestStudentViewSet(APITestCase):
 
-    def test_student_retrieve(self):
-        """Should return a media object when it belongs to the given account"""
+    def test_student_list(self):
+        """Should return the list of all students when requesting /api/students"""
         for _ in range(2):
             StudentFactory()
-        resp = self.client.get('/api/students/')
+        resp = self.client.get('/api/students/?format=json')
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(len(resp.data), 2)
+
+    def test_student_retrieve(self):
+        """Should return the student matching given 'pk' when requesting /api/students/<pk>"""
+        StudentFactory(pk=1, first_name='Martin')
+        resp = self.client.get('/api/students/1/?format=json')
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.data['first_name'], 'Martin')
