a70c637a50947ee4d9ece0bbb535d943ae92655d
Martin Zugnoni <martin.zugnoni@gmail.com>
2015-01-21 10:13:14 -0300

    Set some default DRF configurations

--- students_management/students_management/settings.py
+++ students_management/students_management/settings.py
@@ -85,3 +85,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.AllowAny',
+    ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    )
+}
