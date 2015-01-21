d9d51f95d33049bebfa10880f66fb466eefd3f38
Martin Zugnoni <martin.zugnoni@gmail.com>
2015-01-21 15:28:17 -0300

    Add basic student form with abilty to save new students

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -13,6 +13,29 @@ angular.module('ngStudentsApp')
       $scope.students = students.results;
     });
 
+    $scope.formStudent = {};
+
+    $scope.reset = function () {
+      $scope.formStudent = {};
+    }
+
+    $scope.formatDate = function (date) {
+      return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
+    };
+
+    $scope.save = function () {
+      // convert the date formart to the one expected for the API
+      var date = $scope.formStudent.date_of_birth;
+      $scope.formStudent.date_of_birth = $scope.formatDate(date);
+
+      // create a Student instance and save it through the API
+      var student = new Student($scope.formStudent);
+      student.$save();
+
+      // clean the form
+      $scope.reset();
+    };
+
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
